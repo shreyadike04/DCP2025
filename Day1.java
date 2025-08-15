231e767a2b4f942b922e2106fc6e2caf3b684577
Shreya Dike <shreyadike96@gmail.com>
2025-08-15 20:50:34 +0530

    Create Day1.java
    day1 challenge

---
+++ Day1.java
@@ -0,0 +1,31 @@
+public class Sort012 {
+    public static void sort012(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else { // arr[mid] == 2
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 1, 0, 2, 1, 0};
+        sort012(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
