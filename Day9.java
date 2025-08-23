cbbf5e89f6671595084e81813a3e8e8434a719ec
Shreya Dike <shreyadike96@gmail.com>
2025-08-23 13:24:51 +0530

    Create Day9.java
    Day 9 challenge

---
+++ Day9.java
@@ -0,0 +1,24 @@
+class largecommonstring{
+    public static String longestCommonPrefix(String[] str){
+        if(str == null || str.length== 0 ){
+            return "";
+        }
+        String prefix = str[0];
+        for(int i =1 ; i< str.length; i++){
+            while(str[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() -1);
+
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+
+            }
+        }
+        return prefix;
+    }
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println("Longest Common Prefix: \"" + longestCommonPrefix(strs) + "\"");
+    }
+
+}
